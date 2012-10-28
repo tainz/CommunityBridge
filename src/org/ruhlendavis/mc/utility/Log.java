f6490257a0e1bce7b4ac0b3a1b689b495fad176a
Iain E. Davis <iain@ruhlendavis.org>
2012-10-28 04:57:16 +0000

    * Added string-based setLevel method to Log class.

--- src/org/ruhlendavis/mc/utility/Log.java
+++ src/org/ruhlendavis/mc/utility/Log.java
@@ -30,6 +30,42 @@ public class Log
 		Log.log = log;
 		Log.level = level;
 	}
+
+	public void setLevel(String level)
+	{
+		if (level.equalsIgnoreCase("info"))
+		{
+			this.setLevel(Level.INFO);
+		}
+		else if (level.equalsIgnoreCase("config"))
+		{
+			this.setLevel(Level.CONFIG);
+		}
+		else if (level.equalsIgnoreCase("fine"))
+		{
+			this.setLevel(Level.FINE);
+		}
+		else if (level.equalsIgnoreCase("finer"))
+		{
+			this.setLevel(Level.FINER);
+		}
+		else if (level.equalsIgnoreCase("finest"))
+		{
+			this.setLevel(Level.FINEST);
+		}
+		else if (level.equalsIgnoreCase("all"))
+		{
+			this.setLevel(Level.ALL);
+		}
+		else if (level.equalsIgnoreCase("warning"))
+		{
+			this.setLevel(Level.WARNING);
+		}
+		else if (level.equalsIgnoreCase("severe"))
+		{
+			this.setLevel(Level.SEVERE);
+		}
+	}
 	
 	public void setLevel(Level level)
 	{
