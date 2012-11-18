f86b27a293caca7a11dab37002a367c6be8ae8f3
Iain E. Davis <iain@ruhlendavis.org>
2012-11-18 17:59:14 -0600

    Javadoc cleanup.

--- src/org/ruhlendavis/mc/communitybridge/PermissionHandler.java
+++ src/org/ruhlendavis/mc/communitybridge/PermissionHandler.java
@@ -28,16 +28,17 @@ public interface PermissionHandler
 	 * Retrieves a player's primary group. For some permission systems, this is
 	 * merely the first group on the list.
 	 * 
-	 * @param playerName String containing the player's name
-	 * @return null if the player does not exist or has no groups, otherwise a String containing the group's name.
+	 * @param playerName String containing the player's name.
+	 * @return null if the player does not exist or has no groups, otherwise a
+	 *         String containing the group's name.
 	 */
 	public String getPrimaryGroup(String playerName);
 	
 	/**
 	 *  Determines whether a player is a member of a group.
 	 * 
-	 * @param playerName String containing the player's name
-	 * @param groupName  String containing the group's name
+	 * @param playerName String containing the player's name.
+	 * @param groupName  String containing the group's name.
 	 * @return True only if the player is a member of the group.
 	 */
 	public boolean isMemberOfGroup(String playerName, String groupName);
@@ -45,8 +46,8 @@ public interface PermissionHandler
 	/**
 	 * Determines whether a player has a group has their primary group.
 	 * 
-	 * @param playerName String containing the player's name
-	 * @param groupName  String containing the group's name
+	 * @param playerName String containing the player's name.
+	 * @param groupName  String containing the group's name.
 	 * @return True if the group is the player's primary group.
 	 */
 	public boolean isPrimaryGroup(String playerName, String groupName);
@@ -61,10 +62,11 @@ public interface PermissionHandler
 	public boolean removeFromGroup(String playerName, String groupName);
 
 	/**
-	 * Sets a player's primary group. Note that this may not have any meaning depending on which permissions system is involved.
+	 * Sets a player's primary group. Note that this may not have any meaning
+	 * depending on which permissions system is involved.
 	 * 
-	 * @param playerName String containing player's name to set
-	 * @param groupName  String containing group name to set player's primary group to.
+	 * @param playerName String containing player's name.
+	 * @param groupName  String containing the group name.
 	 * @return true if the set succeeded, false if it failed for any reason.
 	 */
 	public boolean setPrimaryGroup(String playerName, String groupName);
