428632a178e54e99e89ab48edd610ea895daaed3
Iain E. Davis <iain@ruhlendavis.org>
2012-11-18 16:52:56 -0600

    Implemented remaining PermissionHandler methods for PermissionHandlerGroupManager.

--- src/org/ruhlendavis/mc/communitybridge/PermissionHandlerGroupManager.java
+++ src/org/ruhlendavis/mc/communitybridge/PermissionHandlerGroupManager.java
@@ -1,6 +1,9 @@
 package org.ruhlendavis.mc.communitybridge;
 
 import org.anjocaido.groupmanager.GroupManager;
+import org.anjocaido.groupmanager.data.Group;
+import org.anjocaido.groupmanager.data.User;
+import org.anjocaido.groupmanager.dataholder.OverloadedWorldHolder;
 import org.anjocaido.groupmanager.permissions.AnjoPermissionsHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
@@ -47,6 +50,79 @@ public class PermissionHandlerGroupManager implements PermissionHandler
 		groupManager = groupManagerIn;
 	}
 	
+	/**
+	 * Adds a player to a group.
+	 * 
+	 * @param playerName String containing the player's name.
+	 * @param groupName  String containing the group's name.
+	 * @return True if the add succeeded, false if it failed for any reason.
+	 */
+	@Override
+	public boolean addToGroup(String playerName, String groupName)
+	{
+		OverloadedWorldHolder worldHolder = groupManager.getWorldsHolder().getWorldDataByPlayerName(playerName);
+		
+		if (worldHolder == null)
+		{
+			return false;
+    }
+        
+		User user = worldHolder.getUser(playerName);
+		
+		if (user == null)
+		{
+				return false;
+		}
+		
+		Group group = worldHolder.getGroup(groupName);
+    if (group == null)
+		{
+			return false;
+    }
+		
+		// If it is a primary group, set as a primary group.
+		if (user.getGroup().equals(worldHolder.getDefaultGroup()))
+		{
+			user.setGroup(group, true);
+		}
+		else if (group.getInherits().contains(user.getGroup().getName().toLowerCase()))
+		{
+			user.setGroup(group, true);
+		}
+		else
+		{
+			user.addSubGroup(group);
+		}
+
+		return true;
+	}
+
+	/**
+	 * Retrieves an array of group names for the player.
+	 * 
+	 * @param playerName String containing the name of the player.
+	 * @return An String array containing the group names.
+	 */
+	@Override
+	public String[] getGroups(String playerName)
+	{
+		OverloadedWorldHolder worldHolder = groupManager.getWorldsHolder().getWorldDataByPlayerName(playerName);
+		
+		if (worldHolder == null)
+		{
+			return null;
+    }
+        
+		User user = worldHolder.getUser(playerName);
+		
+		if (user == null)
+		{
+				return null;
+		}
+		
+		return user.subGroupListStringCopy().toArray(new String[0]);
+	}
+
 	/**
 	 * Retrieves the primary group for a given player.
 	 * Note that for bPermissions, it returns the first group on the player's
@@ -130,5 +206,69 @@ public class PermissionHandlerGroupManager implements PermissionHandler
 	{
 		String primaryGroup = this.getPrimaryGroup(playerName);
 		return primaryGroup != null && groupName.equalsIgnoreCase(primaryGroup);
+	}
+
+	@Override
+	public boolean removeFromGroup(String playerName, String groupName)
+	{
+		OverloadedWorldHolder worldHolder = groupManager.getWorldsHolder().getWorldDataByPlayerName(playerName);
+		
+		if (worldHolder == null)
+		{
+			return false;
+    }
+        
+		User user = worldHolder.getUser(playerName);
+		
+		if (user == null)
+		{
+				return false;
+		}
+		
+		if (user.getGroup().getName().equalsIgnoreCase(groupName))
+		{
+			user.setGroup(worldHolder.getDefaultGroup(), true);
+			return true;
+		}
+		else
+		{
+			Group group = worldHolder.getGroup(groupName);
+			if (group == null)
+			{
+				return false;
+			}
+			else
+			{
+				return user.removeSubGroup(group);
+			}
+		}
+	}
+
+	@Override
+	public boolean setPrimaryGroup(String playerName, String groupName)
+	{
+		OverloadedWorldHolder worldHolder = groupManager.getWorldsHolder().getWorldDataByPlayerName(playerName);
+		
+		if (worldHolder == null)
+		{
+			return false;
+    }
+        
+		User user = worldHolder.getUser(playerName);
+		
+		if (user == null)
+		{
+				return false;
+		}
+		
+		Group group = worldHolder.getGroup(groupName);
+    if (group == null)
+		{
+			return false;
+    }
+		
+		user.setGroup(group, true);
+
+		return true;
 	}
 }
