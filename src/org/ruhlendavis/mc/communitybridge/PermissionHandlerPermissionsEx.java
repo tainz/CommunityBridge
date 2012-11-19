a85a0bc6fd68ea69d4f0cbaf64f7a59006ac9020
Iain E. Davis <iain@ruhlendavis.org>
2012-11-18 18:09:51 -0600

    Implemented remaining methods for PermissionHandlerPermissionsEx.

--- src/org/ruhlendavis/mc/communitybridge/PermissionHandlerPermissionsEx.java
+++ src/org/ruhlendavis/mc/communitybridge/PermissionHandlerPermissionsEx.java
@@ -3,6 +3,7 @@ package org.ruhlendavis.mc.communitybridge;
 import net.netmanagers.community.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+import ru.tehkode.permissions.PermissionGroup;
 import ru.tehkode.permissions.PermissionUser;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
 
@@ -39,6 +40,49 @@ public class PermissionHandlerPermissionsEx implements PermissionHandler
 	public PermissionHandlerPermissionsEx(boolean dummy) throws IllegalStateException
 	{}
 
+	/**
+	 * Adds a player to a group.
+	 * 
+	 * @param playerName String containing the player's name.
+	 * @param groupName  String containing the group's name.
+	 * @return True if the add succeeded, false if it failed for any reason.
+	 */
+	@Override
+	public boolean addToGroup(String playerName, String groupName)
+	{
+		PermissionGroup group = PermissionsEx.getPermissionManager().getGroup(groupName);
+		PermissionUser user = PermissionsEx.getPermissionManager().getUser(playerName);
+		if (group == null || user == null)
+		{
+			return false;
+		}
+		else
+		{
+			user.addGroup(group);
+			return true;
+		}
+	}
+
+	/**
+	 * Retrieves an array of group names for the player.
+	 * 
+	 * @param playerName String containing the name of the player.
+	 * @return An String array containing the group names.
+	 */
+	@Override
+	public String[] getGroups(String playerName)
+	{
+		return PermissionsEx.getPermissionManager().getUser(playerName).getGroupsNames();
+	}
+
+	/**
+	 * Retrieves a player's primary group. For PermissionsEx this is merely the
+	 * first group on the list.
+	 * 
+	 * @param playerName String containing the player's name
+	 * @return null if the player does not exist or has no groups, otherwise a
+	 *         String containing the group's name.
+	 */
 	@Override
 	public String getPrimaryGroup(String playerName)
 	{
@@ -51,7 +95,7 @@ public class PermissionHandlerPermissionsEx implements PermissionHandler
 		return permissionUser.getGroupsNames()[0];
 	}
 
- /**
+  /**
 	 * Asks permissions system if a player is the member of a given group.
 	 * 
 	 * @param groupName String containing name of group to check
@@ -92,5 +136,34 @@ public class PermissionHandlerPermissionsEx implements PermissionHandler
 	{
 		String primaryGroup = this.getPrimaryGroup(playerName);
 		return primaryGroup != null && groupName.equalsIgnoreCase(primaryGroup);
+	}
+
+	/**
+	 * Removes a player from a group.
+	 * 
+	 * @param playerName String containing the name of the player.
+	 * @param groupName  String containing the name of the group.
+	 * @return True if the removal succeeded, false if it failed for any reason.
+	 */
+	@Override
+	public boolean removeFromGroup(String playerName, String groupName)
+	{
+		PermissionsEx.getPermissionManager().getUser(playerName).removeGroup(groupName);
+		return true;
+	}
+	
+	/**
+	 * Sets a player's primary group. PermissionsEx doesn't have a notion of
+	 * a primary group, so for now this simply calls addToGroup()
+	 * 
+	 * @param playerName String containing player's name.
+	 * @param groupName  String containing the group name.
+	 * @return true if the set succeeded, false if it failed for any reason.
+	 */
+	// TODO: Work out a way to ensure the group is first on the player's group list
+	@Override
+	public boolean setPrimaryGroup(String playerName, String groupName)
+	{
+		return addToGroup(playerName, groupName);
 	}
 }
