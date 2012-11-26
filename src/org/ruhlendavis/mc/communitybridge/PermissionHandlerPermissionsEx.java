e516bf53df498e057df0525ce7c3db6587ad9169
Iain Davis <iain@ruhlendavis.org>
2012-11-26 14:41:50 -0600

    Fix for isMemberOfGroup including inheritance and effectively breaking PEX's demotion system.

--- src/org/ruhlendavis/mc/communitybridge/PermissionHandlerPermissionsEx.java
+++ src/org/ruhlendavis/mc/communitybridge/PermissionHandlerPermissionsEx.java
@@ -7,14 +7,14 @@ import ru.tehkode.permissions.bukkit.PermissionsEx;
 
 /**
  * Implements the PermissionHandler interface for PermissionsEx.
- * 
+ *
  * @author Feaelin
  */
 public class PermissionHandlerPermissionsEx implements PermissionHandler
 {
 	/**
 	 * Actual constructor.
-	 * 
+	 *
 	 * @throws IllegalStateException when PermissionsEx is not present or disabled.
 	 */
 	public PermissionHandlerPermissionsEx() throws IllegalStateException
@@ -30,18 +30,18 @@ public class PermissionHandlerPermissionsEx implements PermissionHandler
 	}
 	/**
 	 * Dummy constructor for unit testing purposes.
-	 * 
+	 *
 	 * @param dummy Any boolean value (not used)
 	 * @throws IllegalStateException Not actually thrown as this is a dummy method
 	 */
 	public PermissionHandlerPermissionsEx(boolean dummy) throws IllegalStateException
 	{}
-	
+
  /**
 	 * Asks permissions system if a player is the member of a given group.
-	 * 
+	 *
 	 * @param groupName String containing name of group to check
-	 * @param player    String containing name of player to check 
+	 * @param player    String containing name of player to check
 	 * @return boolean which is true if the the player is a member of the group
 	 */
 	@Override
@@ -49,13 +49,13 @@ public class PermissionHandlerPermissionsEx implements PermissionHandler
 	{
 		try
 		{
-			return PermissionsEx.getUser(playerName).inGroup(groupName);
+			return PermissionsEx.getUser(playerName).inGroup(groupName, false);
 		}
 		catch (Error e)
 		{
 			Main.log.severe(e.getMessage());
 		}
-		
+
 		return false;
 	}
 }
