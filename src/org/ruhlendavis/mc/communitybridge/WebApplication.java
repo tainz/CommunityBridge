571c18040f2ef9bb4d55e147620871d9cb188ff6
Iain E. Davis <iain@ruhlendavis.org>
2012-12-26 15:52:49 -0600

    Added the new WebApplication class.

---
+++ src/org/ruhlendavis/mc/communitybridge/WebApplication.java
@@ -0,0 +1,18 @@
+package org.ruhlendavis.mc.communitybridge;
+
+/**
+ * Class representing the interface to the web application.
+ *
+ * @author Feaelin (Iain E. Davis) <iain@ruhlendavis.org>
+ */
+public class WebApplication
+{
+	public WebApplication(Configuration config)
+	{
+	}
+
+	public String getUserID(String playerName)
+	{
+		return "";
+	}
+}
