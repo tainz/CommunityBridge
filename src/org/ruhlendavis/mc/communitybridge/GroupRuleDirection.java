15d0dee18c149182b5ee2032396e62c6f07784c4
Iain E. Davis <iain@ruhlendavis.org>
2012-12-29 10:53:40 -0600

    Added the GroupRule class and its corresponding enum.

---
+++ src/org/ruhlendavis/mc/communitybridge/GroupRuleDirection.java
@@ -0,0 +1,12 @@
+package org.ruhlendavis.mc.communitybridge;
+
+/**
+ * Enumeration for possible group synchronization directions
+ *
+ * @author Iain E. Davis <iain@ruhlendavis.org>
+ */
+public enum GroupRuleDirection
+{
+	MINECRAFT,
+	WEBAPP
+}
