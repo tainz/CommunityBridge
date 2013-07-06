459606be9d962c90038fbb692f2b5b15fbacad22
Iain E. Davis <iain@ruhlendavis.org>
2013-07-06 18:12:50 -0500

    Sketched in some of the code for handling vault. Not really even ready for alpha testing yet.

--- src/org/ruhlendavis/mc/communitybridge/PermissionHandlerVault.java
+++ src/org/ruhlendavis/mc/communitybridge/PermissionHandlerVault.java
@@ -1,74 +1,80 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.ruhlendavis.mc.communitybridge;
 
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 /**
+ * Implements the PermissionHandler interface for Vault
+ * Notes about Vault
+ * 2013-July-06: As vault is a gateway to many permissions systems, most of whom
+ *               do not support the notion of a primary group, in effect,
+ *							 vault does not support the notion of a primary group.
  *
  * @author Iain E. Davis <iain@ruhlendavis.org>
  */
 public class PermissionHandlerVault implements PermissionHandler
 {
+	private static Permission vault;
 	public PermissionHandlerVault() throws IllegalStateException
 	{
 		Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("Vault");
 
 		if (plugin == null || plugin.isEnabled())
 		{
-			throw new IllegalStateException("bPermissions is either not present or not enabled.");
+			throw new IllegalStateException("Vault is either not present or not enabled.");
 		}
+		RegisteredServiceProvider<Permission> rsp = Bukkit.getServer().getServicesManager().getRegistration(Permission.class);
+		vault = rsp.getProvider();
 	}
+	
 	@Override
 	public boolean addToGroup(String playerName, String groupName)
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		return vault.playerAddGroup((String)null, playerName, groupName);
 	}
 
 	@Override
 	public String[] getGroups(String playerName)
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		return vault.getPlayerGroups((String) null, playerName);
 	}
 
 	@Override
 	public String[] getGroupsPure(String playerName)
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		return getGroups(playerName);
 	}
 
 	@Override
 	public String getPrimaryGroup(String playerName)
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		return vault.getPrimaryGroup((String) null, playerName);
 	}
 
 	@Override
 	public boolean isMemberOfGroup(String playerName, String groupName)
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		return vault.playerInGroup((String) null, playerName, groupName);
 	}
 
 	@Override
 	public boolean isPrimaryGroup(String playerName, String groupName)
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		throw new UnsupportedOperationException("Not supported yet.");
 	}
 
 	@Override
 	public boolean removeFromGroup(String playerName, String groupName)
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		return vault.playerRemoveGroup((String) null, playerName, groupName);
 	}
 
 	@Override
 	public boolean setPrimaryGroup(String playerName, String groupName, String formerGroupName)
 	{
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		throw new UnsupportedOperationException("Vault does not support setting a primary group.");
 	}
 
 	@Override
