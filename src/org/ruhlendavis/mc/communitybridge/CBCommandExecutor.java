4c4dbdfbcb5db37c92db358c6c9149ee78b2c53e
Iain E. Davis <iain@ruhlendavis.org>
2013-07-07 10:23:43 -0500

    Cleaned up unused imports.

--- src/org/ruhlendavis/mc/communitybridge/CBCommandExecutor.java
+++ src/org/ruhlendavis/mc/communitybridge/CBCommandExecutor.java
@@ -1,9 +1,6 @@
 package org.ruhlendavis.mc.communitybridge;
 
-import java.net.MalformedURLException;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
