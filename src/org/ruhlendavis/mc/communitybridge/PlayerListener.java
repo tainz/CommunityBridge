04fdc71a572c5df0d1a8e0f089b3dc49094dea4b
Iain E. Davis <iain@ruhlendavis.org>
2013-07-10 18:37:27 -0500

    Revised how the move from unregistered group to registered group is handled.

--- src/org/ruhlendavis/mc/communitybridge/PlayerListener.java
+++ src/org/ruhlendavis/mc/communitybridge/PlayerListener.java
@@ -101,17 +101,7 @@ public class PlayerListener implements Listener
 
 			if (!config.groupSynchronizationActive && !config.linkingRegisteredGroup.isEmpty())
 			{
-				if (!config.linkingRegisteredFormerUnregisteredOnly || CommunityBridge.permissionHandler.isMemberOfGroup(playerName, config.linkingUnregisteredGroup) || CommunityBridge.permissionHandler.getGroupsPure(playerName).length == 0)
-				{
-					CommunityBridge.permissionHandler.removeFromGroup(playerName, config.linkingUnregisteredGroup);
-					CommunityBridge.permissionHandler.addToGroup(playerName, config.linkingRegisteredGroup);
-					if (config.linkingNotifyPlayerGroup)
-					{
-						String message = ChatColor.RED + config.messages.get("link-notify-player-group-change");
-						message = message.replace("~GROUPNAME~", config.linkingRegisteredGroup);
-						player.sendMessage(message);
-					}
-				}
+				maybeSwitchToRegistered(playerName, player);
 			}
 			webapp.onJoin(player);
 		}
@@ -146,4 +136,37 @@ public class PlayerListener implements Listener
 	{
 		webapp.onQuit(event.getPlayer());
 	} // onPlayerQuit
+
+	/**
+	 * Checks if changing the player's group from the unregistered group to
+	 * the registered group is merited and does so if it is.
+	 * 
+	 * @param playerName - name of the player to check and possibly move.
+	 * @param player - player object representing the player.
+	 */
+	private void maybeSwitchToRegistered(String playerName, Player player)
+	{
+		// We don't use the linking registered group if it is empty or group
+		// synchronization is active.
+		if (config.groupSynchronizationActive || config.linkingRegisteredGroup.isEmpty())
+		{
+			return;
+		}
+		
+		// if this rule is turned out, we won't change groups unless they're
+		// a member of the unregistered group or they have no groups.
+		if (config.linkingRegisteredFormerUnregisteredOnly && !CommunityBridge.permissionHandler.isMemberOfGroup(playerName, config.linkingUnregisteredGroup) && CommunityBridge.permissionHandler.getGroupsPure(playerName).length != 0)
+		{
+			return;
+		}
+		
+		CommunityBridge.permissionHandler.removeFromGroup(playerName, config.linkingUnregisteredGroup);
+		CommunityBridge.permissionHandler.addToGroup(playerName, config.linkingRegisteredGroup);
+		if (config.linkingNotifyPlayerGroup)
+		{
+			String message = ChatColor.RED + config.messages.get("link-notify-player-group-change");
+			message = message.replace("~GROUPNAME~", config.linkingRegisteredGroup);
+			player.sendMessage(message);
+		}
+	}
 } // PlayerListener class
