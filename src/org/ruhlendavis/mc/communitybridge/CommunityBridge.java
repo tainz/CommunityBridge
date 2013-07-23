a7a9f1cfd6da7c9c54af0ce0342a1972c55f57ec
Iain E. Davis <iain@ruhlendavis.org>
2013-07-23 13:56:15 -0500

    Added code to shutdown metrics cleanly when deactivating. Fixes #46.

--- src/org/ruhlendavis/mc/communitybridge/CommunityBridge.java
+++ src/org/ruhlendavis/mc/communitybridge/CommunityBridge.java
@@ -2,6 +2,7 @@ package org.ruhlendavis.mc.communitybridge;
 
 import java.io.IOException;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 import net.milkbowl.vault.economy.Economy;
 import net.netmanagers.api.SQL;
 import org.bukkit.Bukkit;
@@ -232,6 +233,14 @@ public final class CommunityBridge extends JavaPlugin
 
 		if (metrics != null)
 		{
+			try
+			{
+				metrics.disable();
+			}
+			catch (IOException exception)
+			{
+				log.severe("Exception when disabling metrics: " + exception.getMessage());
+			}
 			metrics = null;
 		}
 
