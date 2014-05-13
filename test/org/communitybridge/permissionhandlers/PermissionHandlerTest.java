9fdd7e5c9f87c02577717e5947b744b488fd8437
Iain E. Davis <iain@ruhlendavis.org>
2014-05-12 23:21:49 -0500

    Refined tests.

--- test/org/communitybridge/permissionhandlers/PermissionHandlerTest.java
+++ test/org/communitybridge/permissionhandlers/PermissionHandlerTest.java
@@ -1,31 +1,17 @@
 package org.communitybridge.permissionhandlers;
 
-import com.avaje.ebean.EbeanServer;
-import java.io.File;
-import java.io.InputStream;
-import java.util.List;
-import java.util.logging.Logger;
-import org.bukkit.Server;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginLoader;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.runner.RunWith;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import org.mockito.runners.MockitoJUnitRunner;
 
-//@RunWith(MockitoJUnitRunner.class)
 public class PermissionHandlerTest
 {
 	private Player player = mock(Player.class);
+	private Plugin plugin = mock(Plugin.class);
 	private final String playerName = "somePlayer";
 	private final String groupOne = "groupOne";
 	private final String groupTwo = "groupTwo";
@@ -57,8 +43,7 @@ public class PermissionHandlerTest
 	@Test
 	public void validateHandlerDoesNotThrowErrorWithValidPlugin()
 	{
-		TestablePlugin plugin = new TestablePlugin();
-		plugin.setEnabled(true);
+		when(plugin.isEnabled()).thenReturn(true);
 
 		String name = "someplugin";
 		try
@@ -74,12 +59,10 @@ public class PermissionHandlerTest
 	@Test
 	public void validateHandlerDoesThrowErrorWithNullPlugin()
 	{
-		Plugin plugin = null;
-
 		String name = "someplugin";
 		try
 		{
-			permissionHandler.validate(plugin, name);
+			permissionHandler.validate(null, name);
 			fail("Failed to throw an exception.");
 		}
 		catch (IllegalStateException exception)
@@ -95,9 +78,7 @@ public class PermissionHandlerTest
 	@Test
 	public void validateHandlerDoesThrowErrorWithDisabledPlugin()
 	{
-		TestablePlugin plugin = new TestablePlugin();
-
-		plugin.setEnabled(false);
+		when(plugin.isEnabled()).thenReturn(false);
 
 		String name = "someplugin";
 		try
@@ -176,148 +157,6 @@ public class PermissionHandlerTest
 
 		@Override
 		public boolean supportsPrimaryGroups()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-	}
-
-	public class TestablePlugin implements Plugin
-	{
-		private boolean enabled = true;
-
-		public void setEnabled(boolean enabled)
-		{
-			this.enabled = enabled;
-		}
-
-		@Override
-		public boolean isEnabled()
-		{
-			return enabled;
-		}
-
-		@Override
-		public File getDataFolder()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public PluginDescriptionFile getDescription()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public FileConfiguration getConfig()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public InputStream getResource(String string)
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public void saveConfig()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public void saveDefaultConfig()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public void saveResource(String string, boolean bln)
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public void reloadConfig()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public PluginLoader getPluginLoader()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public Server getServer()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public void onDisable()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public void onLoad()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public void onEnable()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public boolean isNaggable()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public void setNaggable(boolean bln)
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public EbeanServer getDatabase()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public ChunkGenerator getDefaultWorldGenerator(String string, String string1)
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public Logger getLogger()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public String getName()
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public List<String> onTabComplete(CommandSender cs, Command cmnd, String string, String[] strings)
-		{
-			throw new UnsupportedOperationException("No implementation needed for tests.");
-		}
-
-		@Override
-		public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings)
 		{
 			throw new UnsupportedOperationException("No implementation needed for tests.");
 		}
